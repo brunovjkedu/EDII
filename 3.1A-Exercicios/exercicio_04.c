69e74e5fbbd754a1088085c4ffea95994abaf495
brunovjkedu <bruno.rocha@estudante.uffs.edu.br>
2026-03-10 15:42:04 -0300

    exercicio 04

---
+++ 3.1A-Exercicios/exercicio_04.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    FILE *arquivo;
+    char linha[500];
+    char busca[100];
+
+    // Abrir arquivo
+    arquivo = fopen("arquivo_texto.txt", "r");
+
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    printf("Digite a letra ou palavra que a linha deve comecar: ");
+    fgets(busca, sizeof(busca), stdin);
+
+    // Remove o \n do fgets
+    busca[strcspn(busca, "\n")] = 0;
+
+    printf("\nLinhas que comecam com \"%s\":\n\n", busca);
+
+    // Ler linha por linha
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+
+        // Comparar o inicio da linha
+        if (strncmp(linha, busca, strlen(busca)) == 0) {
+            printf("- %s\n\n", linha);
+        }
+    }
+
+    fclose(arquivo);
+
+    return 0;
+}
